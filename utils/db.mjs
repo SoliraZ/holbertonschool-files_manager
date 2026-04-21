f8f866b447bc2bc680ed92ad8191154d9dbdce22
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-04-21 17:13:42 +0200

    Add MongoDB client utility with connection management and user/file count methods

---
+++ utils/db.mjs
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url);
+    this.db = null;
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch(() => {
+        this.db = null;
+      });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export { DBClient, dbClient };
+export default dbClient;
